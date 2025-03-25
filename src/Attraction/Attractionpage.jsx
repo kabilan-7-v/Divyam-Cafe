c12f71fc5ccba3a9cd6fe64940e1b7c7c30f5417
kabilan-7-v <kabilanvelmani@gmail.com>
2025-03-25 09:11:38 +0530

    changes

--- src/Attraction/Attractionpage.jsx
+++ src/Attraction/Attractionpage.jsx
@@ -11,7 +11,7 @@ function Attractionpage() {
     }, []);
     const fetchPlaces = async () => {
         try {
-            const response = await axios.get('http://localhost:3000/api/getplace');
+            const response = await axios.get('https://divyamcafe-backend.onrender.com/api/getplace');
             console.log(response.data);
 
             // Extract the places array from the response
