1e9ac18ab9110f4216a342b6b89c962a597fe67c
kabilan-7-v <kabilanvelmani@gmail.com>
2025-04-01 22:54:58 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  server: {
+    hmr: {
+      clientPort: 5173, // Replace with your Vite port
+    }
+  }
 })
