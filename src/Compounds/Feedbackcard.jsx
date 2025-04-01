67c7b4e24168f3ac933cf19d6efeca3bd7429274
kabilan-7-v <kabilanvelmani@gmail.com>
2025-04-01 22:12:39 +0530

    changes

--- src/Compounds/Feedbackcard.jsx
+++ src/Compounds/Feedbackcard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Feedbackcard({quote,author,index}) {
   return (
     
-        <div key={index} className='bg-white text-[#603913] md:w-80 p-6 md:ml-12  rounded-lg shadow-md w-40  max-h-70 '>
+        <div key={index} className='bg-white text-[#603913] md:w-80 p-6 md:ml-12  rounded-lg shadow-md w-40  h-70 '>
           <span className='md:text-6xl opacity-10 leading-none text-2xl'>“</span>
           <p className='md:text-sm mb-4 text-[12px]'>{quote}</p>
           <p className='italic font-semibold text-right'>— {author}</p>
